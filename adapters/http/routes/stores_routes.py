4090a8fedc474ea72fda00ea6c6d5629072dd473
Jean Sousa <jean.sousa@vexpenses.com>
2026-02-09 14:39:44 -0300

    add logging

--- adapters/http/routes/stores_routes.py
+++ adapters/http/routes/stores_routes.py
@@ -1,3 +1,4 @@
+import logging
 from fastapi import APIRouter, Depends, HTTPException
 from application.use_cases.get_store import GetStoreUseCase
 from infrastructure.repositories.stores_repository import StoreMemoryRespository
@@ -12,13 +13,17 @@ router = APIRouter(
 )
 
 
+logger = logging.getLogger(__name__)
 store_repo = StoreMemoryRespository()
 
 @router.get("/{store_id}", summary="Consulta uma loja")
 def get_store(store_id: int):
+    logger.info(f"Recebida solicitação de consulta de loja: {store_id}")
     use_case = GetStoreUseCase(store_repo)
     try:
         store = use_case.execute(store_id)
+        logger.info(f"Loja consultada {store_id}: {store}")
         return {"store_id": store_id, "store": store}
     except Exception as e:
+        logger.error(f"Erro ao consultar loja pelo id {store_id}: {e}", exc_info=True)
         raise HTTPException(status_code=404, detail=str(e))
--- main.py
+++ main.py
@@ -1,25 +1,10 @@
-import logging
 from fastapi import FastAPI
 from infrastructure.logging.logging_config import setup_logging
 from adapters.http.routes import stores_routes
 
 setup_logging()
-logger = logging.getLogger(__name__)
 
 app = FastAPI(title="Cadastro de lojas", version="1.0.0")
 
 app.include_router(stores_routes)
-
-
-# @app.get("/stores/{store_id}", summary="Consulta uma loja")
-# def get_store(store_id: int):
-#     logger.info(f"Recebida solicitação de consulta de loja: {store_id}")
-#     use_case = GetStoreUseCase(store_repo)
-#     try:
-#         store = use_case.execute(store_id)
-#         logger.info(f"Loja consultada {store_id}: {store}")
-#         return {"store_id": store_id, "saldo": store}
-#     except Exception as e:
-#         logger.error(f"Erro ao consultar loja pelo id {store_id}: {e}", exc_info=True)
-#         raise HTTPException(status_code=404, detail=str(e))
 
